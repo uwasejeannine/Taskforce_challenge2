a87b11cf28ea3cfc02bfab572f7ba343737f6d65
ciraganenicole <ciraganenicole@gmail.com>
2023-09-14 13:54:21 -0700

    Clean the code

--- server.js
+++ server.js
@@ -7,26 +7,19 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the "public" directory
 app.use(express.static('public'));
 
-// Socket.IO logic
 io.on('connection', (socket) => {
-  console.log('A user connected');
 
-  // Listen for chat messages
   socket.on('chat message', (message) => {
-    // Broadcast the message to all connected clients
     io.emit('chat message', message);
   });
 
-  // Handle disconnection
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 });
 
-// Start the server
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
